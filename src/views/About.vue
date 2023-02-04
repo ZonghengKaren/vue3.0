3b3469b9b082d81126c94d450e12c449e595da3b
ZonghengKaren <31526975+ZonghengKaren@users.noreply.github.com>
2023-02-04 17:50:05 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
       <h1>This is an about page</h1>
     </div>
     <div class="button">
-      <van-button type="default">默认按钮</van-button>
+      <van-button type="default">默认按钮1</van-button>
       <van-button type="primary">主要按钮</van-button>
       <van-button type="info">信息按钮</van-button>
       <van-button type="warning">警告按钮</van-button>
